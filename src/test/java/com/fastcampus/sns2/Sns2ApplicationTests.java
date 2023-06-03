b3be77182e47b4056d354094fa0e04e8a39d2d33
HOME <goueuncom@hotmail.com>
2023-06-03 17:10:44 +0900

    main: create project

---
+++ src/test/java/com/fastcampus/sns2/Sns2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.sns2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sns2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
